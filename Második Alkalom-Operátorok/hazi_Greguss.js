11dcffe083e043e1345a3c644bd0290d64cf94c9
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2022-09-16 13:33:33 +0200

    Update hazi_Greguss.js

--- Második Alkalom-Operátorok/hazi_Greguss.js
+++ Második Alkalom-Operátorok/hazi_Greguss.js
@@ -5,13 +5,17 @@ console.log("Elso hazi");
 
 //document.getElementById("content").innerHTML = `Üdvözlöm ` + u_name;
 
-var a = parseInt( prompt("Kérem az 'a' értékét") );
-var b = parseInt( prompt("Kérem az 'b' értékét") );
+var num1 = parseInt( prompt("Kérem az első szám értékét") );
+var num2 = parseInt( prompt("Kérem az második szám értékét") );
+var biger = num1 <= num2 || num1 >= num2;
+//alert ( `Az ${num1} és ${num2} közül a = ${  num1 <= num2 || num1 >= num2 ? num1 : num2  } a nagyobb`);
 
-//var biger = alert( `A megadott számok közül: ${ biger = ? [${a}<${b}] || [${b}<${a}] } a nagyobb` );
-alert ( `Az "${a}" és "${b}" " közül a " = ${[${a}<${b}] || [${a}>${b}]} a nagyobb`);
+/*if ( num1 <= num2 )
+{
+    alert (`A megadott számok közül ${num2} a nagyobb!`);
+}
+else {
+    alert (`A megadott számok közül ${num1} a nagyobb!`)
+}*/
 
-
-
-
-
+document.getElementById("content").innerHTML = `${a} közül ${b} = ${biger}`;
