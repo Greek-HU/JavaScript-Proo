fc4e40090f0a2c7cc915a7dfc387f629e4a1dd67
Greek-HU <gregusk89@gmail.com>
2022-10-08 12:40:45 +0200

    JavaScript

--- Házik/hazi4.js
+++ Házik/hazi4.js
@@ -1,33 +1,26 @@
 /*
-​
----------- Gyakorlatok Stringekkel -----------------
-!!!!!! Az alábbi feladatokhoz ne használj reguláris kifejezéseket !!!!!!!
-​
     1. Kerjetek be prompt segitsegevel egy nevet Es konzolban jelenitsetek meg ennek a nevnek a monogramjat
        (tehat a kezdobetuit)
 
 var nev = prompt("Kérem adja meg a nevét:");
-
 var nevArray = nev.split(" ");
 var vezNev = nevArray[0].charAt(0);
 var kerNev = nevArray[1].charAt(0);
-//var kerNev = nevArray[]
 console.log(vezNev, kerNev);
+
 //2. kerjetek be egy hosszab szoveget, es konzolban irjatok ki a szoveg mondatainak a szamat.
 var szoveg = prompt("Kérnék egy mondatot:");
-
-console.log(szoveg.split(".","!").length + " sor van a mondatban!");*/
-/*<script>
-const myText = "This is a paragraph. Is it?";
+//console.log(szoveg.split(".","!").length + " sor van a mondatban!");
 const stop = /[.!?]/;
-const sentence = myText.split(stop);
-console.log(sentence.length - 1);
-</script>*/
+const sentence = szoveg.split(stop);
+console.log(sentence.length - 1);*/
+
 //3. Írj függvényt, mely bemeneti paraméterként megkap egy fájlnevet, kimenetként pedig visszatér a fájl kiterjesztésével.
 function fileName(file) {
     return file.split(".")[1];
 }
 console.log(fileName("fájlnév.exe"));
+
 //4. írjatok egy getMonth("2022-10-6") nevezetű függvényt, mely bemenetként megkap egy datum stringet
 //       YYYY-MM-DD dátimformátumban és kiírja a dátumban szereplő hónapot.
 
@@ -62,7 +55,7 @@ var cicus = "Cirmi délben eszik majd alszik egy nagyot. Aztán ha felkelt játs
 /*
     Reguláris kifejezés segítségével szedjük ki a "cicus" változó tartalmból az ik-es igéket.
 */
-console.log(cicus.match(/["ik"?]/));
+console.log(cicus.match(/([A-Za-z], ik*)]/));
 /*
 re.2. irjatok regularis kifejezest, mely egy szovegbol kiveszi a datumido karakterlancokat
  pl 2021.09.28 09:59 (UTC)
