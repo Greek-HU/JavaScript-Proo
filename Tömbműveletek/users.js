0ad469263d6b3e4b21468bbaf4659cf224c6d4e5
Greek-HU <gregusk89@gmail.com>
2022-10-19 14:36:05 +0200

    JavaScript

---
+++ Tömbműveletek/users.js
@@ -0,0 +1,112 @@
+var users = [
+  {
+      "lastName": "Sándor",
+      "firstName": "Ferenc",
+      "username": "ferike",
+      "birthdate": 2002,
+      "email": "ferike@json.hu",
+      "active": true,
+      "status": "user"
+  },
+  {
+      "lastName": "Vass",
+      "firstName": "Melánia",
+      "username": "melike",
+      "birthdate": 1996,
+      "email": "melike@pro.hu",
+      "active": true,
+      "status": "user"
+  },
+  {
+      "lastName": "Solymosi",
+      "firstName": "Aranka",
+      "username": "arika",
+      "birthdate": 1995,
+      "email": "ari@json.hu",
+      "active": false,
+      "status": "admin"
+  },
+  {
+      "lastName": "Hegedűs",
+      "firstName": "Imre",
+      "username": "imi",
+      "birthdate": 1996,
+      "email": "imi@pro.hu",
+      "active": false,
+      "status": "admin"
+  },
+  {
+      "lastName": "László",
+      "firstName": "Tamás",
+      "username": "tami",
+      "birthdate": 1989,
+      "email": "tami@json.hu",
+      "active": true,
+      "status": "user"
+  },
+  {
+      "lastName": "Imrefi",
+      "firstName": "Balázs",
+      "username": "bali",
+      "birthdate": 1996,
+      "email": "bali@pro.hu",
+      "active": false,
+      "status": "admin"
+  },
+  {
+      "lastName": "Hadnagy",
+      "firstName": "Ákos",
+      "username": "akos",
+      "birthdate": 1996,
+      "email": "akos@json.hu",
+      "active": true,
+      "status": "user"
+  },
+  {
+      "lastName": "Csender",
+      "firstName": "Tünde",
+      "username": "tunci",
+      "birthdate": 1996,
+      "email": "tunci@pro.hu",
+      "active": true,
+      "status": "user"
+  },
+  {
+      "lastName": "Orbán",
+      "firstName": "Péter",
+      "username": "peti",
+      "birthdate": 1998,
+      "email": "peti@json.hu",
+      "active": false,
+      "status": "admin"
+  },
+  {
+      "lastName": "Hevesi",
+      "firstName": "Áron",
+      "username": "aron",
+      "birthdate": 1996,
+      "email": "aron@json.hu",
+      "active": false,
+      "status": "user"
+  },
+  {
+      "lastName": "Lakatos",
+      "firstName": "Zoltán",
+      "username": "zoli",
+      "birthdate": 1988,
+      "email": "zoli@pro.hu",
+      "active": true,
+      "status": "user"
+  },
+  {
+      "lastName": "Márton",
+      "firstName": "Kinga",
+      "username": "kinga",
+      "birthdate": 1996,
+      "email": "kinga@json.hu",
+      "active": true,
+      "status": "user"
+  }
+];
+
+export { users };
