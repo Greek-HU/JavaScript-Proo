bf08b9092505336833de10fb4c9cee0bda1ecd52
Greek-HU <gregusk89@gmail.com>
2022-10-20 11:50:21 +0200

    JavaScript

--- Házik/hazi5.js
+++ Házik/hazi5.js
@@ -10,27 +10,43 @@ function szures(users, key, value){
     for (let i = 0; i < users.length; i++) 
         if (users[i][key] == value) 
             res.push(users[i]);
-    
     return res;
+    
     }
+    
     
     var res = szures(users, "lastName", "Vass");
     
     console.log(res);
+    function User(vnev, knev, kor) {
+        this.lastName = vnev;
+        this.firstName = knev;
+        this.age = kor
+    }
 
-
+    User.prototype.bemutatkozas =function(){
+        console.log(users.bemutatkozas());
+        return`A nevem ${this.vnev} ${this.knev} és a korom ${this.kor}!`;
     
+    };/*
     var content = document.querySelector("#elso");
     var gridString = "";
     
-    for (let i = 0; i < users.length; i++) { 
+    for (let i = 0; i < res.length; i++) { 
     
         gridString += '<div class="row">';
     
-            gridString += `<div class="cell">${users[i]}</div>`;
+            gridString += `<div class="cell">${res.bemutatkozas()}</div>`;
         
         gridString += "</div>";
     }
     
-    content.innerHTML += gridString; 
+    content.innerHTML += gridString;
+    
+    
+    
+    
+    var createember1 = new CreateEmber("Horvath", "Aranka", 28);
+    //console.log(createember1.bemutatkozas());
+    document.getElementById("content").innerHTML = ember.bemutatkozas();
     
